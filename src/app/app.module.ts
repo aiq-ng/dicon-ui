eae9b61af412e85eab44a868ceba0b36f6d79228
wobin1 <nathanielmusa3@gmail.com>
2024-11-14 14:56:50 +0100

    minor cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from 'primeng/api';
 import { PrimengModule } from './primeng/primeng.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { PrimengModule } from './primeng/primeng.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/landing-layout/pg-application/pg-application.component.ts
+++ src/app/views/landing-layout/pg-application/pg-application.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './pg-application.component.scss'
 })
 export class PgApplicationComponent {
+  formStage:any;
+  currentStage:any;
+
+
+  ngOnInit(){
+    this.formStage =1;
+    this.currentStage = 1;
+    console.log(this.formStage)
+  }
+
+  toggleFormStage(stage:number){
+    if(stage){
+      this.formStage = stage;
+      this.currentStage = stage;
+    } else {
+      this.formStage = 1;
+    }
+  }
 
 }
