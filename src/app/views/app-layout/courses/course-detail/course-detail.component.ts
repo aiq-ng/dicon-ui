62b8bd07465463615e4b13f5571000f452f1ac0f
unknown <nathanielmusa3@gmail.com>
2024-11-25 10:06:51 +0100

    worked courses flow

--- src/app/views/app-layout/courses/course-detail/course-detail.component.ts
+++ src/app/views/app-layout/courses/course-detail/course-detail.component.ts
@@ -10,6 +10,76 @@ import { MenuItem } from 'primeng/api';
 export class CourseDetailComponent {
   items: MenuItem[] | undefined;
   currentMenu:any = 'courses';
+  students = [
+    {
+      "department": "Global Security and Terrorism",
+      "first_name": "Bello",
+      "last_name": "Ahmed",
+      "next_of_kin": "Jigawa",
+      "state": "Jigawa",
+      "LGA": "Dutse",
+      "address": "Plot no. 116 Lane number 4 Abuja Crescent",
+      "phone_number": "0814412345",
+      "email_address": "student@gmail.com",
+      "course_start": "17 Jan 2023",
+      "course_end": "17 Jan 2023"
+    },
+    {
+      "department": "International Trade and Economics",
+      "first_name": "Grace",
+      "last_name": "Oluwafemi",
+      "next_of_kin": "Lagos",
+      "state": "Lagos",
+      "LGA": "Ikeja",
+      "address": "12 Opebi Road, Ikeja, Lagos",
+      "phone_number": "0810123456",
+      "email_address": "grace.oluwafemi@gmail.com",
+      "course_start": "10 Feb 2023",
+      "course_end": "10 Dec 2023"
+    },
+    {
+      "department": "Human Rights and Humanitarian Intervention",
+      "first_name": "John",
+      "last_name": "Okoro",
+      "next_of_kin": "Enugu",
+      "state": "Enugu",
+      "LGA": "Nsukka",
+      "address": "45 University Road, Nsukka",
+      "phone_number": "0709876543",
+      "email_address": "john.okoro@yahoo.com",
+      "course_start": "5 Mar 2023",
+      "course_end": "5 Mar 2024"
+    },
+    {
+      "department": "Climate Change and Global Governance",
+      "first_name": "Aisha",
+      "last_name": "Abubakar",
+      "next_of_kin": "Kano",
+      "state": "Kano",
+      "LGA": "Nassarawa",
+      "address": "23 Kano Crescent, Nassarawa",
+      "phone_number": "0802233445",
+      "email_address": "aisha.abubakar@hotmail.com",
+      "course_start": "15 Apr 2023",
+      "course_end": "15 Oct 2023"
+    },
+    {
+      "department": "Diplomacy and Conflict Resolution",
+      "first_name": "David",
+      "last_name": "Eze",
+      "next_of_kin": "Abuja",
+      "state": "FCT",
+      "LGA": "Garki",
+      "address": "10 Hospital Road, Garki, Abuja",
+      "phone_number": "0901122334",
+      "email_address": "david.eze@outlook.com",
+      "course_start": "20 May 2023",
+      "course_end": "20 May 2028"
+    }
+  ]
+  
+
+  tableHeader = ['Name', 'Programe', 'phone_number', 'Enrolement date']
 
   constructor(private router:Router) {}
 
